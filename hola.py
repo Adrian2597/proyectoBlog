00b3ab04544d0e732920009654cf7bd1650d2350
Adrian <65386963+Adrian2597@users.noreply.github.com>
2020-05-16 03:31:52 -0500

    Revision Final
    Entregable practica 7 parte 3

--- hola.py
+++ hola.py
@@ -1,12 +1,20 @@
 from flask import Flask #Primero se importa la clase principal
 from flask import render_template #Tambien se importa la carpeta que maneja los templates o plantillas, esta clase
 #buscara el template deseado mediante su nombre en la carpeta templates
+from flask import send_file
 
 app = Flask(__name__) #Instancia de la clase Flask, el primer argumento es el nombre del modulo o paquete de la aplicacon
 
-@app.route("/curriculum/Adrian")
+@app.route("/")
 def url_principal():
-	return render_template("template.html",nombre="Adrián Sánchez Hernández")
+	inicial="proyecto1"
+	imagenes=["proyecto2","proyecto3","proyecto4","proyecto5"]
+	return render_template("template.html",imagenes=imagenes,nombre="Adrián Sánchez Hernández",inicial=inicial)
+
+@app.route("/download")
+def download_file():
+	archivo="static/Document/CurriculumVitaeAdrian.pdf"
+	return send_file(archivo,as_attachment=True)
 
 if __name__ == "__main__":
 	app.run(debug=True)
